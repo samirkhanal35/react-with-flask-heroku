cf4f70cbeb3e7960add5e09bb1cfa3f7b6409ee7
samirkhanal35 <samirkhanal35@gmail.com>
2020-08-27 12:41:37 +0545

    port updated

--- home.py
+++ home.py
@@ -1,18 +1,19 @@
 from flask import Flask,jsonify
 
-flask_app = Flask(__name__, static_folder='./build', static_url_path='/')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 
-@flask_app.route('/')
+@app.route('/')
 def index():
     return app.send_static_file('index.html')
 
-@flask_app.route('/api/time')
+@app.route('/api/time')
 def hello():
     message = jsonify(msg="Hello from flask")
     print(message)
     return message
 
-
+if __name__ == '__main__':
+   app.run()
 
 # import time
 # from flask import Flask
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
-from app.home import app 
-import os
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+from app.main import app 
+  
+if __name__ == "__main__": 
+        app.run() 
